b7eb47e5bd7c8c15163a8ad25e9fb00f575cc13d
suongchan <minhsuong19102002@gmail.com>
2023-09-23 16:16:56 +0700

    register

--- src/main/java/com/example/cinema/controller/AdminController.java
+++ src/main/java/com/example/cinema/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,9 +32,9 @@ public class AdminController {
         return "adminHtml/adminCustomer";
     }
 
-    @GetMapping("/addCustomer")
-    public String showAddCustomerForm(Model model) {
-        model.addAttribute("users", new User());
+    @GetMapping("addCustomer")
+    public String register(@ModelAttribute User user, Model model) {
+        model.addAttribute("user",user);
         return "adminHtml/addCustomer";
     }
 }
